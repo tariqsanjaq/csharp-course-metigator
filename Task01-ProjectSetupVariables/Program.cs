7bfa0d765a61a10b81c8df63785863053d6fd0fd
TariqSanjaq <tariqms.ed@gmail.com>
2026-08-09 12:04:52 +0300

    add task 1 and task 2

---
+++ Task01-ProjectSetupVariables/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+class Program
+{
+    public static void Main(string[] args)
+    {
+        int v1 = default(int);
+        double v2 = default(double);
+        float v3 = default(float);
+        decimal v4 = default(decimal);
+        char v5 = default(char);
+        string v6 = default(string);
+        bool v7 = default(bool);
+        byte v8 = default(byte);
+        long v9 = default(long);
+
+
+
+
+        Console.WriteLine($"default value of int : {v1}   and min value: {int.MinValue} , maxvalue: {int.MaxValue}");
+        Console.WriteLine($"default value of double :{v2} and min value: {double.MinValue}, maxvalue: {double.MaxValue}");
+        Console.WriteLine($"default value of float :{v3}  and min value: {float.MinValue}, maxvalue: {float.MaxValue}");
+        Console.WriteLine($"default value of decimal :{v4}  and min value: {decimal.MinValue}, maxvalue: {decimal.MaxValue}");
+        Console.WriteLine($"default value of char :{v5}  and min value {char.MinValue} maxvalue: {char.MaxValue}");
+        Console.WriteLine($"default value of string :{v6}");
+        Console.WriteLine($"default value of bool :{v7}");
+        Console.WriteLine($"default value of byte:{v8}  and min value: {byte.MinValue} maxvalue: {byte.MaxValue}");
+        Console.WriteLine($"default value of long:{v9}  and min value: {long.MinValue} maxvalue: {long.MaxValue}");
+
+
+
+
+    }
+}
---
+++ Task02-BooleanLogicArrays/Program.cs
@@ -0,0 +1,78 @@
+﻿
+class Program
+{
+    static void Main(string[] args)
+    { // done 1
+      //double? input = null;
+      //Console.WriteLine("Student Grade Checker  ");
+      //Console.WriteLine("-------------------------------------");
+
+        //while (true) 
+        //{
+        //    Console.Write("Enter your grade (Max:4 , Min: 0) : ");
+        //    try
+        //    {
+        //        if (!double.TryParse(Console.ReadLine(), out double value))
+        //        {
+        //            Console.WriteLine("write wrong input value");
+        //            continue;
+        //        }
+        //        input = value;
+        //        if (input > 4 || input < 0)
+        //        {
+        //            Console.WriteLine("Out of range , try again");
+
+        //        }
+        //        else
+        //        {
+        //            break;
+        //        }
+
+        //    }
+        //    catch (Exception ex)
+        //    {
+        //        Console.WriteLine("write wrong input value");
+        //    }
+        //}
+
+
+
+
+
+        //if (input >= 3.65 && input <= 4)   
+        //{
+        //    Console.WriteLine("your grade is Excellent");
+        //}
+        //else if (input >= 3.00 && input <3.65)
+        //{
+        //    Console.WriteLine("your grade is veryGood");
+        //}
+        //else if (input >= 2.50 && input < 3.00)
+        //{
+        //    Console.WriteLine("your grade is good");
+        //}
+        //else if (input >= 2.00 && input < 2.50 )
+        //{
+        //    Console.WriteLine("your grade is Fair");
+        //}
+        //else if (input < 2)
+        //{
+        //    Console.WriteLine("your grade is fail");
+        //}
+        // 2
+
+        int[] value_int = [10];
+        value_int = [1, 2, 4, 5, 6, 7, 90, 6, 554, -33];
+        int? max = value_int[0];
+        for (int i = 0; i < value_int.Length; i++)
+        {
+            if (value_int[i] > max) { max = value_int[i]; }
+
+        }
+        Console.WriteLine(max);
+
+
+    }
+
+
+}
